129e0130af644bee86ab738137a5977604dc2075
bz4552 <glay2500@gmail.com>
2021-08-18 14:46:13 +0000

    投稿機能完成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,11 @@
 class CommentsController < ApplicationController
+
   def create
-    @post = PostImage.find(params[:post_id])
-    comment = current_user.post_comments.new(comment_params)
-    comment.post_id = post.id
+    @post = Post.find(params[:post_id])
+    @comment = post.comments.build(comment_params)
+    comment.user_id = current_user.id
     comment.save
-    redirect_to post_path(post)
+    redirect_to post_path(@post)
   end
 
   def destroy
@@ -15,6 +16,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:comment)
+    params.require(:comment).permit(:body, :post_id)
   end
 end
