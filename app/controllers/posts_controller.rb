098e7b25ac334304a0f48f67dfe6da6183bcf7f8
bz4552 <glay2500@gmail.com>
2021-08-28 11:41:52 +0000

    削除・編集の編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!,except: [:top, :index]
+  before_action :authenticate_user! ,except: [:top, :index]
 
   def new
     @post = Post.new
