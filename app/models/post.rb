fc3a601b7494d92632d1633d18071802efad68f7
bz4552 <glay2500@gmail.com>
2021-09-17 16:25:55 +0000

    一部改良

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :images, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   accepts_attachments_for :images, attachment: :image
 
   validates :title, presence: true
@@ -12,8 +13,7 @@ class Post < ApplicationRecord
   validates :date, presence: true
   validates :line, presence: true
 
-  has_many :favorites, dependent: :destroy
-
+  
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
